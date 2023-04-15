e533545c73d53e6817639c85932c1e2196dc3b71
Max Savonik <savonikmaksim@gmail.com>
2023-04-15 21:04:49 +0300

    fix stylere

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -25,15 +25,15 @@ export const scss = () => {
             groupCssMediaQueries()
         )
     )
-    .pipe(
-        app.plugins.if(
-            app.isBuild,
-            webpcss({
-                webpClass: ".webp",
-                noWebpClass: ".no-webp"
-            })
-        )
-    )
+    // .pipe(
+    //     app.plugins.if(
+    //         app.isBuild,
+    //         webpcss({
+    //             webpClass: ".webp",
+    //             noWebpClass: ".no-webp"
+    //         })
+    //     )
+    // )
     .pipe(
         app.plugins.if(
             app.isBuild,
